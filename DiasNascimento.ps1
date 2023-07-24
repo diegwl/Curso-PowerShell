ae49cbd74f2d46509f94c076a80899fec3fc20b8
Lopes Diego (CaP/ETS) <lod7ca@bosch.com>
2023-07-24 11:18:00 -0300

    first commit

---
+++ DiasNascimento.ps1
@@ -0,0 +1,14 @@
+﻿Clear-Host
+while ($true) {
+    $regex = "^[0-9][0-9][/][0-9][0-9][/][0-9][0-9][0-9][0-9]$"
+    $aniversario = Read-Host "Digite sua data de aniversário"
+    if ($aniversario -notmatch $regex) {
+        Write-Host "Erro, data inválida"
+	break
+    }
+    $diaAniversario = (Get-Date -Date $aniversario)
+    $difereca = (Get-Date).Subtract($diaAniversario).Days
+    Write-Host "Você nasceu fazem " $difereca " dias."
+    break
+}
+Sleep(3)
---
+++ Sorteador.ps1
@@ -0,0 +1,5 @@
+﻿Clear-Host
+$alunos = @("Allana", "Ângelo", "Caio", "Carlos Eduardo", "Diego Alencar", "Diego Castan", "Eduarda", "Esther", "Felipe", "Gabriel Bonaretti", "Giovana Radaeli", "Giovanna Fiuza", "Giovanna Braz", "Henrique", "Julia", "Laura", "Lucas", "Nicole")
+$sorteado = (Get-Random $alunos)
+Write-Host "O Alunos sorteado foi:" $sorteado
+Sleep(7)
