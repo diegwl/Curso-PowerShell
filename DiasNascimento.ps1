5de1bfe155e5b913229f2c0a471694f0ea8910d3
Diego Castan Lopes <127439733+diegwl@users.noreply.github.com>
2023-08-07 10:36:24 -0300

    Update DiasNascimento.ps1

--- DiasNascimento.ps1
+++ DiasNascimento.ps1
@@ -1,6 +1,6 @@
-﻿Clear-Host
+Clear-Host
 while ($true) {
-    $regex = "^[0-9][0-9][/][0-9][0-9][/][0-9][0-9][0-9][0-9]$"
+    $regex = "^[0-9]{0,1}[0-9][/][0-9]{0,1}[0-9][/][0-9][0-9][0-9][0-9]$"
     $aniversario = Read-Host "Digite sua data de aniversário"
     if ($aniversario -notmatch $regex) {
         Write-Host "Erro, data inválida"
